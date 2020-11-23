b17f5d9b4778f43c2db03f10d598937456fb760f
Edgar <mmnledgar@gmail.com>
2020-11-23 19:09:55 +0000

    create_entity_respo

--- api_getech/src/main/java/com/example/securityApp/ApiGetechApplication.java
+++ api_getech/src/main/java/com/example/securityApp/ApiGetechApplication.java
@@ -2,8 +2,10 @@ package com.example.securityApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.example.securityApp.entities","service","controller"})
 public class ApiGetechApplication {
 
 	public static void main(String[] args) {
