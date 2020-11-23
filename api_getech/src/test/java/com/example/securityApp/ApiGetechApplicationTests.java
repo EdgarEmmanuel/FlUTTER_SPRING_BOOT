89ccab08875d28289860740bbbba2e8638bf3e2d
Edgar <mmnledgar@gmail.com>
2020-11-23 18:25:30 +0000

    add_project_spring

---
+++ api_getech/src/test/java/com/example/securityApp/ApiGetechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securityApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGetechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
