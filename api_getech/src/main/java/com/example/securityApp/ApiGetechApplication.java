217e8738decd9443a68278ec9b78bb058e052b54
Edgar <mmnledgar@gmail.com>
2020-11-23 19:03:13 +0000

    add_flutter

---
+++ api_getech/src/main/java/com/example/securityApp/ApiGetechApplication.java
@@ -0,0 +1,13 @@
+package com.example.securityApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGetechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGetechApplication.class, args);
+	}
+
+}
