33b79301c3033e27fa423c9f828da46404501aae
Edgar <mmnledgar@gmail.com>
2021-01-09 21:25:59 +0000

    update request method in front

--- frontfluttertattagine/lib/pages/ResponsableDashboard.dart
+++ frontfluttertattagine/lib/pages/ResponsableDashboard.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'dart:io';
+import 'package:dio/dio.dart' ;
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -31,29 +32,35 @@ class __Respo extends State<ResponsableDashboard>{
   }
 
    login_of_user(String password,String login)async{
-      String url = "http://192.168.1.44:9000/login_respo";
+      String url = "http://192.168.1.8:9000/login_respo";
 
-      Map<String,String> form_data = {
+      var form_data = {
         "email":login,
         "password":password
       };
+      // await http.post(url,
+      //     headers: <String , String>{
+      //       'Content-Type': 'application/json; charset=UTF-8',
+      //     },
+      //     body: jsonEncode(<String,dynamic>{
+      //       "email":login,
+      //       "password":password
+      //     })
+      //   //encoding: Encoding.getByName('utf-8')
+      // );
 
-       await http.post(url,
-          headers: {
-            "Content-Type": "multipart/form-data",
-            'Accept': 'application/json'
-          },
-        body: form_data,
-        //encoding: Encoding.getByName('utf-8')
-      ).then((value) {
-        if(value.body.isNotEmpty){
-          print(value.body);
-          this.ResponsableData = json.decode(value.body);
-        }else{
-          print("an error occured");
-        }
-
-       });
+       await Dio().post(
+          url,
+           data: {
+             "email": login,
+             "password": password
+           },
+         options: new Options(
+           contentType: "application/json"
+         )
+       ).then((value) => (
+        print(value.data)
+       ));
 
   }
 
