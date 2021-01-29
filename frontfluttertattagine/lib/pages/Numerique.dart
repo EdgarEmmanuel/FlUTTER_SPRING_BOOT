f33d8aa5b4bbcea20f1bc0bba97b926a74f22ac1
Edgar <mmnledgar@gmail.com>
2021-01-29 11:01:57 +0000

    finish the nav of the numerique page

--- frontfluttertattagine/lib/pages/Numerique.dart
+++ frontfluttertattagine/lib/pages/Numerique.dart
@@ -23,16 +23,95 @@ class Numerique extends StatelessWidget{
         children: <Widget>[
           Padding(
             padding: EdgeInsets.all(10.0),
-            child: Image.network(""),
-          ),
-          Padding(
-            padding: EdgeInsets.all(10.0),
             child: Text(
-              ""
+              "DEPARTEMENT NUMERIQUE"
             ),
-          )
+          ),
         ],
       ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children:  <Widget>[
+              DrawerHeader(
+                child: Center(
+                  child: ClipRRect(
+                      borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                      child: Container(
+                        child:Image.asset("images/cfp.png"),
+                      )
+                  ),
+                ),
+                decoration: BoxDecoration(
+                  color: Color(0xff3b8391),
+                ),
+              ),
+              ListTile(
+                trailing: Icon(Icons.arrow_right),
+                leading: Icon(Icons.perm_camera_mic),
+                title: Text('Communication digitale'),
+                // onTap: () {
+                //   Navigator.pop(context);
+                //   Navigator.push(context, MaterialPageRoute(
+                //       builder: (context) => Numerique()
+                //   ));
+                // },
+              ),
+              ListTile(
+                trailing: Icon(Icons.arrow_right),
+                leading: Icon(Icons.collections_bookmark),
+                title: Text(
+                    "Marketing digital"
+                ),
+                // onTap: ()=>{
+                //   Navigator.pop(context),
+                //   Navigator.push(context, MaterialPageRoute(
+                //       builder: (context)=>Industrie()
+                //   ))
+                // },
+              ),
+              ListTile(
+                trailing: Icon(Icons.arrow_right),
+                leading: Icon(Icons.desktop_windows),
+                title: Text(
+                    "Développement web"
+                ),
+                // onTap: ()=>{
+                //   Navigator.pop(context),
+                //   Navigator.push(context, MaterialPageRoute(
+                //       builder: (context)=> Gestion()
+                //   ))
+                // },
+              ),
+              ListTile(
+                trailing: Icon(Icons.arrow_right),
+                leading: Icon(Icons.settings_input_antenna),
+                title: Text(
+                    "Réseau informatique"
+                ),
+                // onTap: ()=>{
+                //   Navigator.pop(context),
+                //   Navigator.push(context, MaterialPageRoute(
+                //       builder: (context)=>Tertaire()
+                //   )),
+                // },
+              ),
+              ListTile(
+                trailing: Icon(Icons.arrow_right),
+                leading: Icon(Icons.security),
+                title: Text(
+                    "Maintenance informatique"
+                ),
+                // onTap: ()=>{
+                //   Navigator.pop(context),
+                //   Navigator.push(context, MaterialPageRoute(
+                //       builder: (context)=> Apropos()
+                //   )),
+                // },
+              ),
+            ],
+          ),
+        ),
     );
   }
 
